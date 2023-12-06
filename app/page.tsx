a16635ac5a54856bb56ed2b4202dae3aed2de0e2
howooking <junsgk@gmail.com>
2023-12-07 01:08:32 +0900

    refactor : /wait 페이지 에러 핸들링

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,8 @@ import Navbar from "@/components/home-navbar/navbar";
 export default async function Home() {
   const supabase = await createSupabaseServerClient(true);
 
-  const { data: vet } = await supabase
+  // RLS로 로그인 한 수의사데이터만 가져옴(클라단에서 session정보를 몰라도 된다)
+  const { data: vet, error: vetError } = await supabase
     .from("vets")
     .select("license_approved")
     .single();
@@ -19,10 +20,17 @@ export default async function Home() {
     redirect("/hospital");
   }
 
+  if (vetError) {
+    // vet이 null 이 경우 vetError가 발생하며 code가 "PGRST116"임
+    if (vetError.code !== "PGRST116") {
+      throw new Error("error while fetching vet data in home page");
+    }
+  }
+
   return (
     <>
       <Navbar />
-      <h1 className="pt-20">수의사 전문차트 서비스 벳터핸즈입니다.</h1>
+      <h1 className="pt-16">안녕하세요</h1>
     </>
   );
 }
