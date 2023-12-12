50d61f5f5afe7e4abac5dacde38ba78839d36da4
howooking <junsgk@gmail.com>
2023-12-12 10:43:54 +0900

    refactor : 로그인 후 redirect 오류 수정

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -8,21 +8,39 @@ export async function GET(request: Request) {
 
   if (code) {
     const supabase = await createSupabaseServerClient();
-    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    const { error: sessionError } = await supabase.auth.exchangeCodeForSession(
+      code
+    );
 
-    const { data } = await supabase
+    if (sessionError) {
+      return NextResponse.redirect(`${requestUrl.origin}/auth-error`);
+    }
+
+    // RLS : authenticated, auth.uid() = vet_id
+    const { data: vet, error: vetError } = await supabase
       .from("vets")
       .select("default_hos_id")
       .single();
 
-    const defaultHospitalId = data?.default_hos_id;
-    const redirectURL = defaultHospitalId
-      ? `${requestUrl.origin}/hospital/${defaultHospitalId}`
-      : `${requestUrl.origin}/new-hospital`;
-
-    if (!error) {
-      return NextResponse.redirect(redirectURL);
+    if (vetError) {
+      // 회원이 아닌 경우 vetError.code === "PGRST116"가 발생
+      if (vetError.code !== "PGRST116")
+        return NextResponse.redirect(`${requestUrl.origin}/auth-error`);
     }
+
+    // 회원이 아닌경우, vet === null
+    if (!vet) {
+      return NextResponse.redirect(`${requestUrl.origin}/signup`);
+    }
+
+    // 기본 병원 설정이 안된 경우, vet.default_hos_id === null
+    if (!vet.default_hos_id) {
+      return NextResponse.redirect(`${requestUrl.origin}/new-hospital`);
+    }
+
+    // 기본 병원 설정이 된 경우, vet.default_hos_id !== null
+    return NextResponse.redirect(
+      `${requestUrl.origin}/hospital/${vet.default_hos_id}`
+    );
   }
-  return NextResponse.redirect(`${requestUrl.origin}/auth-error`);
 }
