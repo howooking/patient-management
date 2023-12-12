255404b3e9591ae418117edc593b602287b99480
howooking <junsgk@gmail.com>
2023-12-12 19:39:26 +0900

    feat : 미들웨어 catch

--- middleware.ts
+++ middleware.ts
@@ -48,3 +48,15 @@ export async function middleware(request: NextRequest) {
 
   return response;
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!_next/static|_next/image|favicon.ico).*)",
+  ],
+};
