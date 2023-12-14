7821602a93ae56b9c2c35650852f4b9d04793314
howooking <junsgk@gmail.com>
2023-12-14 20:30:58 +0900

    fix : 0년 표시 안되게

--- lib/helper-function/pet-age.ts
+++ lib/helper-function/pet-age.ts
@@ -17,7 +17,9 @@ export default function calculateAge(inputDate: string) {
 
   months += months < 0 ? 12 : 0;
 
-  const ageString = `${years}년${months > 0 ? ` ${months}개월` : ""}`;
+  const ageString = `${years > 0 ? `${years}년` : ""}${
+    months > 0 ? ` ${months}개월` : ""
+  }`;
 
   return ageString;
 }
